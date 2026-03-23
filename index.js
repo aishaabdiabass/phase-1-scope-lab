e5001f6862fff0e7aec33b8935f08cdbf348bdc7
aisha abdi <aishaabdi1878@gmail.com>
2026-03-23 14:00:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Declare and manipulate globals as required by the scope lab
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = 'billy';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'chris';
+}
